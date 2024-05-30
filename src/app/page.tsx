9f1dcc037056e2fcfe5ddad016c047bc17be8ac7
Kaikei-e <e_kaikei@protonmail.com>
2024-05-30 21:05:47 +0900

    Added the three schema mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
-        <p>CBAid</p>
+        <h1>CBAid</h1>
+        <div className="flex flex-row">
+          <h1>Healthy Adult</h1>
+          <h1>Vulnerable Child</h1>
+          <h1>Critical Parent</h1>
+        </div>
       </div>
     </main>
   );
