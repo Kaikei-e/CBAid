5d00edda1b657c8d4497488e6fea79df32f37bf2
Kaikei-e <e_kaikei@protonmail.com>
2024-06-06 22:32:24 +0900

    removed the unnecessary flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import { CriticalParent } from "@/app/SchemaModes/CriticalParent";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <div className="z-10 w-full flex-col items-center justify-center font-mono text-sm lg:flex">
-        <p className="m-2 text-3xl">CBAid</p>
-        <h1 className="m-2 text-xl">Schema Modes</h1>
+      <div className="z-10 w-full h-5/6 flex-col items-center justify-center font-mono text-sm ">
+        <p className="m-2 text-3xl text-center">CBAid</p>
+        <h1 className="m-2 text-xl text-center">Schema Modes</h1>
         <div className="schema-modes flex mt-4 flex-row w-full h-5/6 justify-between">
           <HealthyAdult />
           <VulnerableChild />
